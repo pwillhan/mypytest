8dc7512993ba69da4d9d7d480c1aebf5a6580e41
William Han <whan@godaddy.com>
2022-09-28 16:26:57 -0700

    add request test

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,2 +1,18 @@
+import requests
+
+
 def test_foo():
     assert 4 / 2 == 2
+
+
+def test_get_request():
+    url = "https://httpbin.org/get"
+
+    payload = {}
+    headers = {
+        'accept': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+    assert response.status_code == 200
